5814daf708f65e4ad8319de26987909e82b6f583
pyynb <52124938+pyynb@users.noreply.github.com>
2024-06-21 13:32:25 +0800

    Update create_dev_conda_env.sh

--- script/create_dev_conda_env.sh
+++ script/create_dev_conda_env.sh
@@ -174,7 +174,7 @@ if [[ -z "${dry_run}" ]]; then
   if [[ -n "${force_create}" ]]; then
     conda_args="${conda_args} --force "
   fi
-  cp /tmp/${rand} /script/conda_env.yaml
+  cp ${yaml_path} /script/conda_env.yaml
 else
   echo "Running in dry mode, so creation of conda environment is skipped."
 fi
